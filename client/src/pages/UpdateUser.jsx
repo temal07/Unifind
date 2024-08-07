51bc6d94bdae774a554b719d894de1920d38e596
temal07 <temiralimov38@gmail.com>
2024-08-07 14:36:32 -0400

    finish uploading profile picture

--- client/src/pages/UpdateUser.jsx
+++ client/src/pages/UpdateUser.jsx
@@ -1,21 +1,81 @@
-import { Label ,Avatar, Button, TextInput, Alert } from 'flowbite-react';
-import React, { useState, useEffect } from 'react'
+import { Label ,Avatar, Button, TextInput, Alert, Textarea } from 'flowbite-react';
+import React, { useState, useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
-import { updateFailure, updateStart, updateSuccess } from '../redux/user/userSlice';
+import { updateFailure, updateStart, updateSuccess } from '../redux/user/userSlice.js';
+import { app } from '../firebase.js';
+import { 
+    getStorage, 
+    getDownloadURL, 
+    ref, 
+    uploadBytesResumable
+} from 'firebase/storage';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function UpdateUser() {
     const [formData, setFormData] = useState({});
-    const { currentUser } = useSelector((state) => state.user);
+    // extract currentUser, the error field and loading field from the state.user state
+    const { currentUser, error, loading } = useSelector((state) => state.user);
     const [updateUserError, setUpdateUserError] = useState(null);
     const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+    const [imageFileUploadError, setImageFileUploadError] = useState(null);
+    const [imageFileUploading, setImageFileUploading] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const filePickerRef = useRef();
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    // Handles Image changes
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImageFile(file);
+            setImageFileUrl(URL.createObjectURL(file));
+        }
+    }
+
     useEffect(() => {
-        console.log(formData);
-    }, []); 
+        if (imageFile) {
+            handleUploadImage()
+        }
+    }, [imageFile]);
+
+    const handleUploadImage = async () => {
+        setImageFileUploading(true);
+        setImageFileUploadError(null);
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + imageFile.name;
+        const storageRef = ref(storage,  fileName);
+        const uploadTask = uploadBytesResumable(storageRef, imageFile);
+        uploadTask.on('state_changed', 
+            (snapshot) => {
+                // gets the percentage (e.g. image is 30% uploaded)
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setImageFileUploadProgress(progress.toFixed(0));
+            },
+            (error) => {
+                setImageFileUploadError(
+                    'Could not upload image (File must be less than 2MB)'
+                );
+                setImageFileUploadProgress(null);
+                setImageFileUrl(null);
+                setImageFileUploading(false);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+                    setImageFileUrl(downloadUrl);
+                    setFormData({...formData, profilePicture: downloadUrl});
+                    setImageFileUploading(false);
+                });
+            }
+        );
+    };
 
     // Populate formData with currentUser data when component mounts
     useEffect(() => {
@@ -49,7 +109,11 @@ export default function UpdateUser() {
             return;
         }
 
-        setUpdateUserError(null);
+        if (imageFileUploading) {
+            setUpdateUserError('Please wait for the image to upload');
+            return;
+        }
+
         try {
             dispatch(updateStart());
             const res = await fetch(`/api/user/update-user/${currentUser._id}`, {
@@ -89,11 +153,48 @@ export default function UpdateUser() {
                             <h1 className='text-xl text-gray-400'>Profile Picture</h1>
                         </div>
                         <hr className='border-0 bg-gray-400 h-0.5 w-9/12 mx-0 my-0'/>
-                        <img
-                            src={currentUser.profilePicture}
-                            alt='user'
-                            className={`rounded-full w-48 h-48 object-cover border-4 border-[lightgray]`}
-                        />
+                        <input 
+                            type='file'
+                            accept='image/*'
+                            onChange={handleImageChange}
+                            ref={filePickerRef}
+                            hidden
+                        /> 
+                        <div
+                            className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
+                            onClick={() => filePickerRef.current.click()}
+                        >
+                            {imageFileUploadProgress && (
+                                <CircularProgressbar
+                                value={imageFileUploadProgress || 0}
+                                text={`${imageFileUploadProgress}%`}
+                                strokeWidth={5}
+                                styles={{
+                                    root: {
+                                    width: '100%',
+                                    height: '100%',
+                                    position: 'absolute',
+                                    top: 0,
+                                    left: 0,
+                                    },
+                                    path: {
+                                    stroke: `rgba(62, 152, 199, ${
+                                        imageFileUploadProgress / 100
+                                    })`,
+                                    },
+                                }}
+                                />
+                            )}
+                            <img
+                                src={imageFileUrl || currentUser.profilePicture}
+                                alt='user'
+                                className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+                                imageFileUploadProgress &&
+                                imageFileUploadProgress < 100 &&
+                                'opacity-60'
+                                }`}
+                            />
+                        </div>
                         <h1 className='text-lg text-gray-400'>(Click on the profile picture to update)</h1>
                         <div className=''>
                             <h1 className='text-xl text-gray-400'>About Account</h1>
@@ -222,7 +323,7 @@ export default function UpdateUser() {
                             value={formData.sat}
                         />
                         <Label value='Your Extracurriculars:' />
-                        <TextInput 
+                        <Textarea
                             type='text'
                             placeholder='Extracurriculars:'
                             required
